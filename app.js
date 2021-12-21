eeb3377701b246a8ea658c27a7ec1b13cae9f834
Houston Chapman <chapmanh10@gmail.com>
2021-12-21 15:14:20 -0500

    page template dynamic

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
- 
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const generatePage = require('./src/page-template.js');
 
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs)
+const [person, github] = profileDataArgs;
 
 
+fs.writeFile('index.html', generatePage(person, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
